0b7f39918ff9582c9fb7fe8324bea3ab587bab0d
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-05-28 13:42:15 +0400

    🚧 Display all native balance divisions

--- src/domain/portfolio/ui/demo.rs
+++ src/domain/portfolio/ui/demo.rs
@@ -1,16 +1,14 @@
 use {
-	crate::near_account::{get_balance, BalanceQueryResponse, NearAccountId},
+	crate::near_account::{get_balance, BalanceQueryResponse, NearAccountBalance, NearAccountId},
 	leptos::*,
 };
 
 #[component]
 pub fn PortfolioDemo(#[prop(into)] account_id: String) -> impl IntoView {
-	let account_id_signal = create_rw_signal(account_id);
+	let account_id_signal = create_rw_signal(NearAccountId(account_id));
 
-	let balance_resource: Resource<String, BalanceQueryResponse> =
-		create_resource(account_id_signal, |arg0: std::string::String| {
-			get_balance(NearAccountId(arg0))
-		});
+	let balance_resource: Resource<NearAccountId, BalanceQueryResponse> =
+		create_resource(account_id_signal, get_balance);
 
 	view! {
 		<Transition fallback={move || {
@@ -20,10 +18,13 @@ pub fn PortfolioDemo(#[prop(into)] account_id: String) -> impl IntoView {
 				balance_resource
 					.get()
 					.map(|balance| {
+						let NearAccountBalance { available, total, staked, state_staked } = balance
+							.unwrap()
+							.unwrap();
 						view! {
 							<div border={"2 solid rounded-lg"} w={"128"} flex={"~ col"}>
 								<h1 class={"typography"} m={"0"} p={"4"} border-b={"2 solid"}>
-									{format!("{}'s balance", account_id_signal.get().to_string())}
+									{format!("{}'s balance", account_id_signal.get().0)}
 								</h1>
 
 								<ul
@@ -34,13 +35,83 @@ pub fn PortfolioDemo(#[prop(into)] account_id: String) -> impl IntoView {
 									gap={"3"}
 									list={"none"}
 								>
-									<li flex={"~"} items={"center"} gap={"1"}>
-										<span class={"typography"} text={"lg"} font={"bold"}>
+									<li flex={"~"} items={"center"} gap={"2"}>
+										<span
+											class={"typography"}
+											text={"lg"}
+											font={"bold"}
+											w={"64"}
+										>
+											{"Available:"}
+										</span>
+
+										<span class={"typography"} w={"100%"} text={"center"}>
+											{format!(
+												"{:.8} Ⓝ",
+												available.parse::<f32>().unwrap_or(0.0)
+													/ 1000000000000000000000000.0,
+											)
+												.to_string()}
+										</span>
+									</li>
+
+									<li flex={"~"} items={"center"} gap={"2"}>
+										<span
+											class={"typography"}
+											text={"lg"}
+											font={"bold"}
+											w={"64"}
+										>
+											{"Locked for storage:"}
+										</span>
+
+										<span class={"typography"} w={"100%"} text={"center"}>
+											{format!(
+												"{:.8} Ⓝ",
+												state_staked.parse::<f32>().unwrap_or(0.0)
+													/ 1000000000000000000000000.0,
+											)
+												.to_string()}
+										</span>
+									</li>
+
+									<li flex={"~"} items={"center"} gap={"2"}>
+										<span
+											class={"typography"}
+											text={"lg"}
+											font={"bold"}
+											w={"64"}
+										>
+											{"Staked:"}
+										</span>
+
+										<span class={"typography"} w={"100%"} text={"center"}>
+											{format!(
+												"{:.8} Ⓝ",
+												staked.parse::<f32>().unwrap_or(0.0)
+													/ 1000000000000000000000000.0,
+											)
+												.to_string()}
+										</span>
+									</li>
+
+									<li flex={"~"} items={"center"} gap={"2"}>
+										<span
+											class={"typography"}
+											text={"lg"}
+											font={"bold"}
+											w={"64"}
+										>
 											{"Total:"}
 										</span>
 
 										<span class={"typography"} w={"100%"} text={"center"}>
-											{balance.unwrap().unwrap().total}
+											{format!(
+												"{:.8} Ⓝ",
+												total.parse::<f32>().unwrap_or(0.0)
+													/ 1000000000000000000000000.0,
+											)
+												.to_string()}
 										</span>
 									</li>
 								</ul>
